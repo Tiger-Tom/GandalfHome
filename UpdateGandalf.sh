fcb93885c3add0c8ba1a45c187aa74080d7b57aa
TigerTom <55163690+Tiger-Tom@users.noreply.github.com>
2021-04-21 16:43:38 -0700

    Add files via upload

--- UpdateGandalf.sh
+++ UpdateGandalf.sh
@@ -9,6 +9,25 @@ if [ -d ~/Gandalf ]; then
 	sudo rm -r !(_config)
 	cd $source/Gandalf/
 	cp -r !(_config) ~/Gandalf/
+	#echo "Copied files"
+	#echo "Checking for missing configuration..."
+	#foundConfigs=()
+	#for entry in ~/Gandalf/_config/*
+	#do
+	#	echo "Found entry $entry..."
+	#	foundConfigs+=( $entry )
+	#done
+	#for entry in $source/Gandalf/_config/*
+	#do
+	#	echo "Found entry $entry in source"
+	#	if [[ " ${foundConfigs[@]} " =~ " $entry " ]]; then
+	#		echo "$entry exists"
+	#	else
+	#		echo "$entry does not exist"
+	#		cp $entry ~/Gandalf/_config
+	#		echo "Copied $entry"
+	#	fi
+	#done
 	cd ~/Gandalf/
 else
 	echo "No previous installation found. Installing defaults..."
